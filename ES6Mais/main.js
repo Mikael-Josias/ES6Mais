7c354b396f9a9ad5ef3825842d5b51b4e8740dfe
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-01 13:07:56 -0300

    4.Class

--- ES6Mais/main.js
+++ ES6Mais/main.js
@@ -1,11 +1,39 @@
-alert('teste');
-
-class Teste{
-    metodo(){
-        
+class List{
+    constructor(){
+        this.data = [];
     }
 
-    outroMetodo(){
-        
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Diego';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var minhaLista = new TodoList();
+
+document.getElementById('novoTodo').onclick = function(){
+    minhaLista.add('NovoTodo');
+}
+
+
+//////////////////////////////////////////////////
+
+class Matematica{
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(1, 2));
