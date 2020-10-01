72253899261f6567a1bc7529e14f2dddc3acb4f1
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-01 15:42:02 -0300

    11.Template Literals

--- ES6Mais/main.js
+++ ES6Mais/main.js
@@ -1,42 +1,6 @@
-//REST
-const usuario = {
-    nome: "Diego",
-    idade: 23,
-    empresa: "RocketSeat",
-}
+const nome = "Diego";
+const idade = 23;
 
-const {nome, ...resto} = usuario;
+console.log("Meu nome é "+ nome + " e tenho " + idade + " anos.")
 
-console.log(nome);
-console.log(resto);
-
-
-const arr = [1, 2, 3, 4, 5];
-
-const {a, b, ...c} = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma (...params){
-    return params.reduce((total, next) => (total + next));
-}
-
-
-//SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuario1 = {
-    nome: "Mikael",
-    idade: 18,
-    empresa: "RocketSeat",
-};
-
-const usuario2 = {...usuario1, nome: "Diego", idade: 23};
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
