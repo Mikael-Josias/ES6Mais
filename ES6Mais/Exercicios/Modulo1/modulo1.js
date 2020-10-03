0e25f333b547f9c976cd0bd1cc8a706120e2c831
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-03 13:14:52 -0300

    4th Exercise

--- ES6Mais/Exercicios/Modulo1/modulo1.js
+++ ES6Mais/Exercicios/Modulo1/modulo1.js
@@ -1,25 +1,20 @@
-const usuarios = [  
-    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },  
-    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },  
-    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-];
+const empresa = {  
+    nome: 'Rocketseat',  
+    endereco: {    
+        cidade: 'Rio do Sul',    
+        estado: 'SC',  
+    }
+};
 
+const {nome, endereco: {cidade, estado}} = empresa;
 
-const mapUsu = usuarios.map(item => item.idade);
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+//
+//
+function mostraInfo({nome, idade}){
+    return `${nome} tem ${idade} anos.`;
+}
 
-console.log(mapUsu);
-
-const filterUsu = usuarios.filter(item => (item.empresa == 'Rocketseat' && item.idade > 18));
-
-console.log(filterUsu);
-
-const findUsu = usuarios.find(item => item.empresa === "Google");
-
-console.log(findUsu);
-
-const uniOpUsu = (usuarios.map(function(item){
-    item.idade *= 2;
-    return item;
-})).filter(item => item.idade <= 50);
-
-console.log(uniOpUsu);
+mostraInfo({nome: "Diego", idade: 23});
