9bea1195e8ca2912ba68b86492ee177e6d7921b9
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-03 13:37:02 -0300

    6th Exercise

--- ES6Mais/Exercicios/Modulo1/modulo1.js
+++ ES6Mais/Exercicios/Modulo1/modulo1.js
@@ -1,30 +1,4 @@
-const arr = [1, 2, 3, 4, 5, 6];
+const usuario = 'Diego';
+const idade = 23;
 
-const [x, ...y] = arr;
-
-console.log(x, y);
-
-
-function soma(...params){
-    return params.reduce((total, next) => (total + next))
-}
-
-console.log(soma(1,2,3,4,5,6));
-//
-//
-const usuario = {  
-    nome: 'Diego',  
-    idade: 23,  
-    endereco: {    
-        cidade: 'Rio do Sul',    
-        uf: 'SC',    
-        pais: 'Brasil',  
-    }
-};
-
-var usuario2 = {...usuario, nome: 'Gabriel'};
-
-var usuario3 = {...usuario, endereco: {cidade: 'Lontras'}};
-
-console.log(usuario2);
-console.log(usuario3);
+console.log(`O usuário ${usuario} possui ${idade} anos`);
