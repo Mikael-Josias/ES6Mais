58aa819a9083be6f2704b7510a19732f5d2813f0
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-05 13:16:08 -0300

    2.Import and Export

--- ES6Mais/main.js
+++ ES6Mais/main.js
@@ -1,3 +1,14 @@
-import {soma} from './funcoes';
+//import {soma, sub} from './funcoes';
+import mult, { soma as somaFuncao, sub as subFuncao} from './funcoes';
+// import * as funcoes from './funcoes';
 
-console.log(soma(1, 2));
+//console.log(funcoes.soma(1, 2));
+console.log(somaFuncao(1, 2));
+console.log(subFuncao(1, 2));
+console.log(mult(1, 2));
+
+import somaFunction from './soma';
+
+console.log(somaFunction(1, 2));
+
+
