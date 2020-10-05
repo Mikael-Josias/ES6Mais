7153e8d873303bb58389fe21e3cad1c653c70306
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-05 13:56:06 -0300

    3.Webpack Dev Server

--- ES6Mais/webpack.config.js
+++ ES6Mais/webpack.config.js
@@ -1,15 +1,22 @@
 module.exports = {
-    entry: './main.js',
+    entry: './src/main.js',
     output: {
-        path: __dirname,
+        path: __dirname + '/public',
         filename: 'bundle.js',
     },
+
+    devServer: {
+        contentBase: __dirname + '/public',
+    },
+
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                }
             },
         ],
     },
