bb09995cce5b41d228c165826d14ad2166885f29
Mikael Josias <mikaeljosias2002@gmail.com>
2020-10-06 17:06:55 -0300

    1st Exercise

---
+++ ES6Mais/Exercicios/Modulo3/modulo3.js
@@ -0,0 +1,15 @@
+
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+async function umPorSegundo() 
+{  
+    await delay();
+    console.log('1s');
+    await delay();
+    console.log('2s');
+    await delay();
+    console.log('3s');
+}
+    
+umPorSegundo();
